bb29efd0a19250236e6c2217aa25feca2affd1da
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-06-18 20:24:50 +0200

    Register and unregister sensor and location updates

--- app/src/main/java/net/saidijamnig/healthapp/CompassActivity.java
+++ app/src/main/java/net/saidijamnig/healthapp/CompassActivity.java
@@ -1,18 +1,21 @@
 package net.saidijamnig.healthapp;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 
-public class CompassActivity extends AppCompatActivity {
+public class CompassActivity extends AppCompatActivity implements SensorEventListener, LocationListener{
     private ImageView compassImage;
     private TextView orientationTextView, gpsOrientationTextView, altitudeTextView,
             latitudeTextView, longitudeTextView, brightnessTextView;
@@ -36,5 +39,28 @@ public class CompassActivity extends AppCompatActivity {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION_PERMISSION);
+                return;
+            }
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+            sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    0, 0, this);
+        }
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            sensorManager.unregisterListener(this);
+            locationManager.removeUpdates(this);
+        }
     }
 }
