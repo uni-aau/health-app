dbeaca3a45b7eef863066fbe957fe22769cfe53b
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-06-18 20:32:50 +0200

    Resolved errors

--- app/src/main/java/net/saidijamnig/healthapp/CompassActivity.java
+++ app/src/main/java/net/saidijamnig/healthapp/CompassActivity.java
@@ -1,5 +1,6 @@
 package net.saidijamnig.healthapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
@@ -10,14 +11,15 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-
-public class CompassActivity extends AppCompatActivity implements SensorEventListener, LocationListener{
+public class CompassActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
     private ImageView compassImage;
     private TextView orientationTextView, gpsOrientationTextView, altitudeTextView,
             latitudeTextView, longitudeTextView, brightnessTextView;
@@ -48,65 +50,94 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+            return;
+        }
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0, 0, this);
+    }
 
     @Override
-        protected void onResume() {
-            super.onResume();
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        REQUEST_LOCATION_PERMISSION);
-                return;
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            gravity = event.values;
+        }
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            geomagnetic = event.values;
+        }
+        if (gravity != null && geomagnetic != null) {
+            float[] rotationMatrix = new float[9];
+            boolean success = SensorManager.getRotationMatrix(rotationMatrix,
+                    null, gravity, geomagnetic);
+            if (success) {
+                float[] orientation = new float[3];
+                SensorManager.getOrientation(rotationMatrix, orientation);
+                azimuth = (float) Math.toDegrees(orientation[0]);
+                azimuth = (azimuth + 360) % 360;
+
+                Animation anim = new RotateAnimation(-currentAzimuth, -azimuth,
+                        Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                currentAzimuth = azimuth;
+
+                anim.setDuration(500);
+                anim.setRepeatCount(0);
+                anim.setFillAfter(true);
+
+                compassImage.startAnimation(anim);
             }
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-            sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    0, 0, this);
         }
+    }
 
-        @Override
-        protected void onPause() {
-            super.onPause();
-            sensorManager.unregisterListener(this);
-            locationManager.removeUpdates(this);
-        }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                gravity = event.values;
-            }
-            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                geomagnetic = event.values;
-            }
-            if (gravity != null && geomagnetic != null) {
-            }
-        }
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        double altitude = location.getAltitude();
 
-        @Override
-        public void onLocationChanged(Location location) {
+        orientationTextView.setText("Orientation: " + String.format("%.2f", azimuth) + "°");
+        gpsOrientationTextView.setText("GPS Orientation: " + getDirection(azimuth));
+        altitudeTextView.setText("Altitude: " + String.format("%.2f", altitude) + "m");
+        latitudeTextView.setText("Latitude: " + String.format("%.6f", latitude));
+        longitudeTextView.setText("Longitude: " + String.format("%.6f", longitude));
 
-        }
+        // TODO: Get brightness value and update brightnessTextView
+    }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
 
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
 
-        @Override
-        public void onProviderEnabled(String provider) {
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 
     private String getDirection(float azimuth) {
@@ -120,5 +151,5 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
             return "West";
         }
     }
-
 }
+
