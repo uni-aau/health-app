28e1156fd1b85e17996ebfe9f80b093399acd9b0
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-06-18 20:15:57 +0200

    Implement temporary button click actions

--- app/src/main/java/net/saidijamnig/healthapp/MainActivity.java
+++ app/src/main/java/net/saidijamnig/healthapp/MainActivity.java
@@ -2,7 +2,9 @@ package net.saidijamnig.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //When needed
+    /*public void openHealthActivity(View view) {
+        Intent intent = new Intent(this, HealthActivity.class);
+        startActivity(intent);
+    }*/
+
+    //When needed
+
+    /*public void openGPSActivity(View view) {
+        Intent intent = new Intent(this, GPSActivity.class);
+        startActivity(intent);
+    }*/
+
+    //When needed
+    /*public void openCompassActivity(View view) {
+        Intent intent = new Intent(this, CompassActivity.class);
+        startActivity(intent);
+    }*/
+
+    //When needed
+    /*public void openHistoryActivity(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }*/
 }
