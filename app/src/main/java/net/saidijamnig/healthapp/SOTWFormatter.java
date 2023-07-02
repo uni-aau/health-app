6e83d4661f66357f9047b1099bdfd1408bd25c59
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-07-02 18:24:25 +0200

    Add localization support to SOTWFormatter

---
+++ app/src/main/java/net/saidijamnig/healthapp/SOTWFormatter.java
@@ -0,0 +1,65 @@
+package net.saidijamnig.healthapp;
+
+import android.content.Context;
+
+public class SOTWFormatter {
+    private static final int[] sides = {0, 45, 90, 135, 180, 225, 270, 315, 360};
+    private static String[] names = null;
+
+    public SOTWFormatter(Context context) {
+        initLocalizedNames(context);
+    }
+
+    public String format(float azimuth) {
+        int iAzimuth = (int)azimuth;
+        int index = findClosestIndex(iAzimuth);
+        return iAzimuth + "° " + names[index];
+    }
+
+    private void initLocalizedNames(Context context) {
+
+        if (names == null) {
+            names = new String[]{
+                    context.getString(R.string.sotw_north),
+                    context.getString(R.string.sotw_northeast),
+                    context.getString(R.string.sotw_east),
+                    context.getString(R.string.sotw_southeast),
+                    context.getString(R.string.sotw_south),
+                    context.getString(R.string.sotw_southwest),
+                    context.getString(R.string.sotw_west),
+                    context.getString(R.string.sotw_northwest),
+                    context.getString(R.string.sotw_north)
+            };
+        }
+    }
+
+    private static int findClosestIndex(int target) {
+
+        int i = 0, j = sides.length, mid = 0;
+        while (i < j) {
+            mid = (i + j) / 2;
+
+            if (target < sides[mid]) {
+
+                if (mid > 0 && target > sides[mid - 1]) {
+                    return getClosest(mid - 1, mid, target);
+                }
+                j = mid;
+            } else {
+                if (mid < sides.length-1 && target < sides[mid + 1]) {
+                    return getClosest(mid, mid + 1, target);
+                }
+                i = mid + 1;
+            }
+        }
+
+        return mid;
+    }
+
+    private static int getClosest(int index1, int index2, int target) {
+        if (target - sides[index1] >= sides[index2] - target) {
+            return index2;
+        }
+        return index1;
+    }
+}
