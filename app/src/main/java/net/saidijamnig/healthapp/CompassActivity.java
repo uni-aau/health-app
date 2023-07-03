c6ea6c13eb30e9987887c2ae19850d1a55567f2f
Daadii <dadi1@speed.at>
2023-07-03 20:03:00 +0200

    added compass activity

---
+++ app/src/main/java/net/saidijamnig/healthapp/CompassActivity.java
@@ -0,0 +1,14 @@
+package net.saidijamnig.healthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+    }
+}
