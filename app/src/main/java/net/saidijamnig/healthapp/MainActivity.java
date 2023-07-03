73197efc15e3627b59f14be3f6bca24990cb93e0
Daadii <dadi1@speed.at>
2023-07-03 20:02:36 +0200

    implemented setup for activity switching via navbar

--- app/src/main/java/net/saidijamnig/healthapp/MainActivity.java
+++ app/src/main/java/net/saidijamnig/healthapp/MainActivity.java
@@ -1,10 +1,14 @@
 package net.saidijamnig.healthapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,31 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchIntents();
     }
 
-    //When needed
-    /*public void openHealthActivity(View view) {
-        Intent intent = new Intent(this, HealthActivity.class);
-        startActivity(intent);
-    }*/
+    private void switchIntents() {
+        BottomNavigationView bottomView = findViewById(R.id.bottomNavigationView);
+        bottomView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Intent intent = null;
 
-    //When needed
+                if (id == R.id.health) {
+                    intent = new Intent(MainActivity.this, MainActivity.class);
+                } else if (id == R.id.gps) {
+                    intent = new Intent(MainActivity.this, MainActivity.class);
+                } else if (id == R.id.compass) {
+                    intent = new Intent(MainActivity.this, MainActivity.class);
+                } else if (id == R.id.history) {
+                    intent = new Intent(MainActivity.this, MainActivity.class);
+                }
 
-    /*public void openGPSActivity(View view) {
-        Intent intent = new Intent(this, GPSActivity.class);
-        startActivity(intent);
-    }*/
+                if (intent != null) {
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
 
-    //When needed
-    /*public void openCompassActivity(View view) {
-        Intent intent = new Intent(this, CompassActivity.class);
-        startActivity(intent);
-    }*/
 
-    //When needed
-    /*public void openHistoryActivity(View view) {
-        Intent intent = new Intent(this, HistoryActivity.class);
-        startActivity(intent);
-    }*/
-    // test
+    }
 }
