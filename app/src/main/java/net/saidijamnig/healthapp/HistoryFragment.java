8da9ccfbcf21210b93c21293d178d898fafbc4b7
Daadii <dadi1@speed.at>
2023-07-03 21:05:58 +0200

    reformatted code

--- app/src/main/java/net/saidijamnig/healthapp/HistoryFragment.java
+++ app/src/main/java/net/saidijamnig/healthapp/HistoryFragment.java
@@ -1,12 +1,11 @@
 package net.saidijamnig.healthapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class HistoryFragment extends Fragment {
     public HistoryFragment() {
--- app/src/main/java/net/saidijamnig/healthapp/MainActivity.java
+++ app/src/main/java/net/saidijamnig/healthapp/MainActivity.java
@@ -1,17 +1,11 @@
 package net.saidijamnig.healthapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 import net.saidijamnig.healthapp.databinding.ActivityMainBinding;
 
