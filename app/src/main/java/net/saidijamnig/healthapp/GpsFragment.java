92a30e5b8da77b0c89bd2d7cf35cb1e33741dd82
Daadii <dadi1@speed.at>
2023-07-06 15:06:53 +0200

    added map zoom when gps is found

--- app/src/main/java/net/saidijamnig/healthapp/GpsFragment.java
+++ app/src/main/java/net/saidijamnig/healthapp/GpsFragment.java
@@ -1,17 +1,33 @@
 package net.saidijamnig.healthapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.LongDef;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import net.saidijamnig.healthapp.databinding.FragmentGpsBinding;
 
@@ -19,6 +35,8 @@ import java.util.Locale;
 
 public class GpsFragment extends Fragment implements OnMapReadyCallback {
     private double totalDistance = 0.0;
+    private GoogleMap mMap;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
     private int totalCalories = 0;
     private int seconds = 0;
     private int minutes = 0;
@@ -28,6 +46,7 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback {
     private TextView durationTV, distanceTV, caloriesTV;
     private Button startTrackingButton, stopTrackingButton;
     private FragmentGpsBinding binding;
+    private LocationManager locationManager;
 
 
     public GpsFragment() {
@@ -72,9 +91,41 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback {
         System.out.println("Stop Tracking works fine!");
     }
 
+    @SuppressLint("MissingPermission") // TODO
+    // Todo check mit toast, ob gps location existiert
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        if (getRequiredPermissions()) {
+            mMap = googleMap;
+            mMap.setMyLocationEnabled(true);
 
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastKnownLocation != null) {
+                double latitude = lastKnownLocation.getLatitude();
+                double longitude = lastKnownLocation.getLongitude();
+
+                Log.d("TAG", "Latitude = " + latitude + " longitude = " + longitude);
+
+                LatLng currentLocation = new LatLng(latitude, longitude);
+
+                mMap.addMarker(new MarkerOptions().position(currentLocation).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f));
+            }
+        } else {
+            Log.e("TAG", "Error resolving permissions for onMapReady - Not granted");
+            Toast.makeText(requireContext(), "You need to grant permission to access location!", Toast.LENGTH_SHORT).show();
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSION);}
+
+
+    }
+
+    // Todo extract everything regarding permission to permission handler and rework it
+    private boolean getRequiredPermissions() {
+        // Check location permissions
+        return ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
     private void initializeStartValues() {
