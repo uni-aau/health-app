0bc4f2daac21a9977859a553142bf10042855c2b
Daadii <dadi1@speed.at>
2023-07-06 14:23:44 +0200

    added basic value initialization and implementation for gps activity

--- app/src/main/java/net/saidijamnig/healthapp/GpsFragment.java
+++ app/src/main/java/net/saidijamnig/healthapp/GpsFragment.java
@@ -4,10 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-public class GpsFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import net.saidijamnig.healthapp.databinding.FragmentGpsBinding;
+
+public class GpsFragment extends Fragment implements OnMapReadyCallback {
+    private double totalDistance = 0.0;
+    private int totalCalories = 0;
+    private int seconds = 0;
+    private int minutes = 0;
+    private int hours = 0;
+    private boolean isTracking = false;
+    private SupportMapFragment mapFragment;
+    private TextView durationTV, distanceTV, caloriesTV;
+    private Button startTrackingButton, stopTrackingButton;
+    private FragmentGpsBinding binding;
+
 
     public GpsFragment() {
         // Required empty public constructor
@@ -19,9 +38,51 @@ public class GpsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentGpsBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        durationTV = binding.gpsTextviewDurationStatus;
+        distanceTV = binding.gpsTextviewDistance;
+        caloriesTV = binding.gpsTextviewCalories;
+        startTrackingButton = binding.buttonGpsStart;
+        startTrackingButton.setOnClickListener(view1 -> startTracking());
+        stopTrackingButton = binding.buttonGpsStop;
+        stopTrackingButton.setOnClickListener(view1 -> stopTracking());
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        initializeStartValues();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gps, container, false);
+        return view;
+    }
+
+    int test = 0;
+
+    private void startTracking() {
+        durationTV.setText(Integer.toString(test));
+        test++;
+        System.out.println("Start Tracking works fine!");
+    }
+
+    private void stopTracking() {
+        System.out.println("Stop Tracking works fine!");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    private void initializeStartValues() {
+        String durationStatus = getString(R.string.text_gps_duration_status);
+        String formattedDurationStatus = String.format(durationStatus, String.valueOf(hours), String.valueOf(minutes), String.valueOf(seconds));
+        durationTV.setText(formattedDurationStatus);
+
+        String formattedCalories = String.format(getString(R.string.text_gps_calories), String.valueOf(totalCalories));
+        caloriesTV.setText(formattedCalories);
+        String formattedDistance = String.format(getString(R.string.text_gps_distance), String.valueOf(totalDistance));
+        distanceTV.setText(formattedDistance);
     }
 }
