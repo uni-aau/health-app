0aa160a909d02b82e6258477350a20de903afcae
Daadii <dadi1@speed.at>
2023-07-11 22:03:57 +0200

    added first simple HistoryDao interface with few queries

---
+++ app/src/main/java/net/saidijamnig/healthapp/database/HistoryDao.java
@@ -0,0 +1,27 @@
+package net.saidijamnig.healthapp.database;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+/* Provides methods that rest of the app uses to interact
+ * with data in the History Table
+ */
+@Dao
+public interface HistoryDao {
+    @Query("SELECT * FROM history")
+    List<History> getWholeHistoryEntries();
+
+    @Insert
+    void insertNewHistoryEntry();
+
+    @Delete
+    void dropHistoryEntryById(int uid);
+
+    @Delete
+    void dropWholeHistoryTable();
+}
