65ccfe79510b2ad8e798208f3afd18f486c49c21
Daadii <dadi1@speed.at>
2023-07-11 21:58:42 +0200

    added History database table in Room

---
+++ app/src/main/java/net/saidijamnig/healthapp/database/History.java
@@ -0,0 +1,24 @@
+package net.saidijamnig.healthapp.database;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// Defines schema for the GPS Activity History
+@Entity
+public class History {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "activity_date")
+    public String activityDate;
+
+    @ColumnInfo(name = "duration_status")
+    public String durationStatus;
+
+    @ColumnInfo(name = "activity_distance")
+    public String activityDistance;
+
+    @ColumnInfo(name = "activity_calories")
+    public String activityCalories;
+}
