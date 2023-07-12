e78a6587711d96bc5fbb849694e2630f0215e148
Daadii <dadi1@speed.at>
2023-07-12 17:13:32 +0200

    implemented proper value setting for recyclerview

--- app/src/main/java/net/saidijamnig/healthapp/HistoryListAdapter.java
+++ app/src/main/java/net/saidijamnig/healthapp/HistoryListAdapter.java
@@ -2,15 +2,18 @@ package net.saidijamnig.healthapp;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import net.saidijamnig.healthapp.database.History;
+import net.saidijamnig.healthapp.databinding.RecyclerViewHistoryBinding;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class HistoryListAdapter extends RecyclerView.Adapter<HistoryListAdapter.CustomViewHolder> {
     private final ArrayList<History> list;
@@ -25,13 +28,50 @@ public class HistoryListAdapter extends RecyclerView.Adapter<HistoryListAdapter.
     @NonNull
     @Override
     public HistoryListAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_history, parent, false);
-        return new CustomViewHolder(view);
+        RecyclerViewHistoryBinding binding = RecyclerViewHistoryBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new CustomViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryListAdapter.CustomViewHolder holder, int position) {
+        History history = list.get(position);
 
+        TextView durationTv = holder.binding.recviewTextviewDuration;
+        TextView typeTv = holder.binding.recviewTextviewType;
+        TextView dateTv = holder.binding.recviewTextviewDate;
+        TextView distanceTv = holder.binding.recviewTextviewDistance;
+        ImageView activityTrack = holder.binding.recviewImageTrack;
+
+        int durationInMilliSeconds = Integer.parseInt(history.durationInMilliSeconds);
+        durationTv.setText(formatDuration(durationInMilliSeconds));
+
+        dateTv.setText(history.activityDate);
+
+        String unformattedTypeString = context.getString(R.string.text_history_type);
+        String formattedTypeString = String.format(unformattedTypeString, "-/-"); // TODO
+        typeTv.setText(formattedTypeString);
+
+        String unformattedDistanceString = context.getString(R.string.text_history_distance);
+        String formattedDistanceString = String.format(unformattedDistanceString, history.activityDistance);
+        distanceTv.setText(formattedDistanceString);
+
+        // TODO Bildsetzung
+    }
+
+    private String formatDuration(int durationInMilliSeconds) {
+        int hours = (durationInMilliSeconds / (1000 * 60 * 60)) % 24;
+        int minutes = (durationInMilliSeconds / (1000 * 60)) % 60;
+        int seconds = (durationInMilliSeconds / 1000) % 60;
+
+
+        String unformattedDurationString = context.getString(R.string.text_history_duration);
+        return String.format(unformattedDurationString, formatTime(hours), formatTime(minutes), formatTime(seconds));
+    }
+
+
+    // TODO auslagern in timeformatter klasse (auch gps)
+    private String formatTime(int value) {
+        return String.format(Locale.getDefault(), "%02d", value); // two digits and the leading is a zero if necessary
     }
 
     @Override
@@ -40,11 +80,11 @@ public class HistoryListAdapter extends RecyclerView.Adapter<HistoryListAdapter.
     }
 
     class CustomViewHolder extends RecyclerView.ViewHolder {
-        private final View layoutView;
+        private final RecyclerViewHistoryBinding binding;
 
-        public CustomViewHolder(View layoutView) {
-            super(layoutView);
-            this.layoutView = layoutView;
+        public CustomViewHolder(RecyclerViewHistoryBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }
