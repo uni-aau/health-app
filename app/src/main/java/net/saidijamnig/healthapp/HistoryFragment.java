7fd5625bf911a7541f821c6c04a0ec3bef84ecf8
Daadii <dadi1@speed.at>
2023-07-12 15:23:23 +0200

    implemented history list adapter in HistoryFragment

--- app/src/main/java/net/saidijamnig/healthapp/HistoryFragment.java
+++ app/src/main/java/net/saidijamnig/healthapp/HistoryFragment.java
@@ -5,9 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import net.saidijamnig.healthapp.databinding.FragmentHistoryBinding;
+
+import java.util.ArrayList;
 
 public class HistoryFragment extends Fragment {
+    private FragmentHistoryBinding binding;
+    public static ArrayList<HistoryListElement> historyElements = new ArrayList<>();
     public HistoryFragment() {
         // Required empty public constructor
     }
@@ -22,5 +32,22 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        sendDataToRecyclerView();
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void sendDataToRecyclerView() {
+        HistoryListAdapter viewAdapter = new HistoryListAdapter(historyElements, requireContext());
+        RecyclerView.LayoutManager viewManager = new LinearLayoutManager(requireContext());
+
+        RecyclerView recyclerView = binding.historyRecyclerView;
+        recyclerView.setLayoutManager(viewManager);
+        recyclerView.setAdapter(viewAdapter);
+
     }
 }
