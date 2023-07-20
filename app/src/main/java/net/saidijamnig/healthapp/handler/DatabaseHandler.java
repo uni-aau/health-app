3b89f7a19016e4b620c0554af6390de68c46ecd2
Daadii <dadi1@speed.at>
2023-07-20 15:28:08 +0200

    added databaseHandler with singleton pattern to remove redundant code

---
+++ app/src/main/java/net/saidijamnig/healthapp/handler/DatabaseHandler.java
@@ -0,0 +1,32 @@
+package net.saidijamnig.healthapp.handler;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import net.saidijamnig.healthapp.database.AppDatabase;
+
+public class DatabaseHandler {
+    private static AppDatabase historyDb;
+
+    private DatabaseHandler() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static synchronized AppDatabase getInitializedHistoryDatabase(Context context) {
+        if (historyDb == null) {
+            historyDb = Room.databaseBuilder(context, AppDatabase.class, "history")
+                    .fallbackToDestructiveMigration() // Deletes whole database when version gets changed
+                    .build();
+        }
+        return historyDb;
+    }
+
+    public static synchronized void closeHistoryDatabase() {
+        if(historyDb != null) {
+            historyDb.close();
+        }
+    }
+
+
+}
