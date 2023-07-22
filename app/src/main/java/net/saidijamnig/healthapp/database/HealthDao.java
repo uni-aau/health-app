b40bb55fba80e523ba279784fcec9fa81b6103dd
Daadii <dadi1@speed.at>
2023-07-22 17:25:58 +0200

    added health database table

---
+++ app/src/main/java/net/saidijamnig/healthapp/database/HealthDao.java
@@ -0,0 +1,11 @@
+package net.saidijamnig.healthapp.database;
+
+import androidx.room.Dao;
+
+/**
+ * Provides methods that rest of the app uses to interact
+ * with data in the Health Table
+ */
+@Dao
+public interface HealthDao {
+}
